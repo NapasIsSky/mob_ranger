af15d65ba1ab65938a2147c71fcb85478f5fc44b
NAPAS KAVALEE <napas.kavalee@gmail.com>
2019-12-20 13:17:36 +0700

    test button

--- mob/src/App.js
+++ mob/src/App.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import  'antd/dist/antd.css'
-import {Row , Col, Button} from 'antd'
-function App() {
-  return (
-    <div style={{position:"absolute", left: '50%', top:'50%',}}>
-      <Row type='flex' justify='center' >
-        <Col><Button>BLUE</Button></Col>  
-        <Col><Button>RED</Button></Col>  
-        <Col><Button>YELLOW</Button></Col>  
-      </Row>
-    </div>
-  );
+import "./App.css";
+import "antd/dist/antd.css";
+import { Row, Col, Button } from "antd";
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundColor: "green"
+    };
+  }
+
+handleClick=(backgroundColor)=>()=>{
+ 
+  this.setState({
+    backgroundColor : backgroundColor
+  })
 }
 
-export default App;
+  render() {
+    return (
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: this.state.backgroundColor,
+          width:'100vw',
+          height:'100vh'
+        }}
+      >
+        <Row type="flex" justify="center" align='middle'>
+          <Col >
+            <Button onClick = {this.handleClick('blue')}>BLUE</Button>
+          </Col>
+          <Col>
+            <Button>RED</Button>
+          </Col>
+          <Col>
+            <Button>YELLOW</Button>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
